5ed76616c41ca63c3c727f480beb7d090dccd602
Yelissey Balanov <iamveider@gmail.com>
2019-07-04 15:53:36 +0600

    project initialized

---
+++ template-/config/tasks.js
@@ -0,0 +1,9 @@
+module.exports = [
+    './template-/tasks/pug',
+    './template-/tasks/sass',
+    './template-/tasks/serve',
+    './template-/tasks/watch',
+    './template-/tasks/script',
+    './template-/tasks/img',
+    './template-/tasks/fonts'
+];
